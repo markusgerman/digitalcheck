f38d2accd638b292ca96bc67715273b9c5368b6f
markusgerman <german@myconsult.de>
2020-08-13 16:27:12 +0200

    Dashboard counter
    Dashboard implementiert

--- digital_check/basic_app/views.py
+++ digital_check/basic_app/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+from .models import FillDashboard
+
 # Create your views here.
 
 def home(request):
+
+    diagramm = FillDashboard()
+    ku = diagramm.getallumfrageteilnehmer("ku")
+    kmu = diagramm.getallumfrageteilnehmer("kmu")
+
+    umfragenMonat = diagramm.getumfrageteilnehmerlastweek()
+
+    umfragenGesamt = ku + kmu
     
-    return render(request, 'home.html',)
+
+    context = {
+        'ku' : ku,
+        'kmu' : kmu,
+        'umfragenM' : umfragenMonat,
+        'umfragenG' : umfragenGesamt,
+    }
+    
+    return render(request, 'home.html', context)
